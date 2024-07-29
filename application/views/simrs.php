cd38d60ccb98d14f9322f8d528955e7a98668f2f
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-07-29 12:00:52 +0700

    simrs1 KELAR RESPONSIF

---
+++ application/views/simrs.php
@@ -0,0 +1,76 @@
+<div class="container-fluid simrs">
+    <div class="row">
+        <div class="col-md-2 p-0"></div>
+        <div class="col-md p-0 simrs1">
+            <div class="simrs-judul text-md-start d-flex justify-content-between mx-3">
+                <h2>SIMRS TECHNO MEDIC.</h2>
+                <p>Sistem komprehensif yang mendukung berbagai departemen rumah sakit dari pendaftaran hingga back
+                    office, termasuk Poliklinik, IGD, Rawat Inap, Gizi, Laundry, IPSRS, Kepegawaian, dan Akuntansi.
+                    Terintegrasi dengan BPJS, KEMENKES, LIS, PACS, H2H dengan Bank, dan aplikasi lainnya sesuai
+                    kebutuhan.</p>
+            </div>
+            <div class="simrs-konten simrsk1">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div
+                            class="col-md-4 p-0 d-flex flex-column justify-content-between justify-content-md-center align-items-center row1">
+                            <div class="box ">
+                                <h5>50 Modul</h5>
+                                <p>Pelayanan
+                                    Non Pelayanan</p>
+                            </div>
+                            <div class="box ">
+                                <h5>Web Based</h5>
+                                <p>Support Device PC/Mobile</p>
+                            </div>
+                            <div class="box ">
+                                <h5>Integrasi</h5>
+                                <p>Modul saling terintegrasi
+                                    Integrasi pihak ketiga</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4 p-0 d-flex flex-md-column justify-content-center align-items-center row2">
+                            <div class="boximg">
+                                <img src="<?= base_url('assets/image/simrs.png'); ?>" alt="simrs">
+                            </div>
+                        </div>
+                        <div
+                            class="col-md-4 p-0 d-flex flex-column justify-content-between justify-content-md-center align-items-center row3">
+                            <div class="box ">
+                                <h5>EMR/FHIR</h5>
+                                <p>Electronic Medical Record</p>
+                            </div>
+                            <div class="box ">
+                                <h5>RBAC</h5>
+                                <p>Role Based Access Control</p>
+                            </div>
+                            <div class="box ">
+                                <h5>Comply</h5>
+                                <p>Hak Cipta, SPE,
+                                    Penyelenggara ERM</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2 p-0"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-2 p-0"></div>
+        <div class="col-md p-0 simrs2">
+            <div class="simrs-judul text-md-start d-flex justify-content-between mx-3">
+                <h2>SIMRS TECHNO MEDIC.</h2>
+                <h3>Modul</h3>
+            </div>
+            <div class="simrs-konten">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum maxime placeat culpa. Earum molestias
+                    temporibus voluptatum! Molestiae laboriosam voluptatum reprehenderit? Temporibus mollitia, voluptate
+                    dignissimos expedita laboriosam quia necessitatibus architecto facere.
+                </p>
+            </div>
+        </div>
+        <div class="col-md-2 p-0"></div>
+    </div>
+</div>
