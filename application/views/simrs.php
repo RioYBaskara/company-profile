8b03fd7e7795ee16e5a762199e405e4411862710
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-07-29 16:10:36 +0700

    Terakhir udah selesai tinggal modul lainnya di paling bawah

--- application/views/simrs.php
+++ application/views/simrs.php
@@ -63,12 +63,176 @@
                 <h2>SIMRS TECHNO MEDIC.</h2>
                 <h3>Modul</h3>
             </div>
-            <div class="simrs-konten">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum maxime placeat culpa. Earum molestias
-                    temporibus voluptatum! Molestiae laboriosam voluptatum reprehenderit? Temporibus mollitia, voluptate
-                    dignissimos expedita laboriosam quia necessitatibus architecto facere.
-                </p>
+            <div class="simrs-konten simrsk2">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div
+                            class="col-6 col-xl-3 py-0 d-flex flex-column justify-content-between justify-content-md-center align-items-center baris">
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/pendaftaran.png'); ?>" alt="Pendaftaran">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Pendaftaran</h2>
+                                    <p>Online, Onsite, MJKN, Android/iOS.</p>
+                                </div>
+                            </div>
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/pelayanan.png'); ?>" alt="Pelayanan">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Pelayanan</h2>
+                                    <p>Rawat Jalan, Rawat Inap, Rawat Intensif, VK.</p>
+                                </div>
+                            </div>
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/laboratorium.png'); ?>" alt="Laboratorium">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Laboratorium</h2>
+                                    <p>Order, Hasil, LIS, LOINC.</p>
+                                </div>
+                            </div>
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/radiologi.png'); ?>" alt="Radiologi">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Radiologi</h2>
+                                    <p>Order, Hasil, PACS, LOINC.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            class="col-6 col-xl-3 py-0 d-flex flex-column justify-content-between justify-content-md-center align-items-center baris">
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/farmasi.png'); ?>" alt="Farmasi">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Farmasi</h2>
+                                    <p>Resep, Pengadaan, Distribusi, Stok Opname.</p>
+                                </div>
+                            </div>
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/bedah.png'); ?>" alt="Bedah">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Bedah</h2>
+                                    <p>Tindakan, BHP, Resep, Laporan OP.</p>
+                                </div>
+                            </div>
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/rekammedis.png'); ?>" alt="Rekam Medis">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Rekam Medis</h2>
+                                    <p>EMR, ICD, Coding, INACBGS.</p>
+                                </div>
+                            </div>
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/kasir.png'); ?>" alt="Kasir">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Kasir</h2>
+                                    <p>Payment, Transfer, H2H, QRIS.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            class="col-6 col-xl-3 py-0 d-flex flex-column justify-content-between justify-content-md-center align-items-center baris">
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/kepegawaian.png'); ?>" alt="Kepegawaian">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Kepegawaian</h2>
+                                    <p>Personalia, SIP, STR, Jabatan, dll.</p>
+                                </div>
+                            </div>
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/keuangan.png'); ?>" alt="Keuangan">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Keuangan</h2>
+                                    <p>Perencanaan, Pengadaan, Akuntansi.</p>
+                                </div>
+                            </div>
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/gizi.png'); ?>" alt="Gizi">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Gizi</h2>
+                                    <p>Resep, Pengadaan, Distribusi, Stok Opname.</p>
+                                </div>
+                            </div>
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/kerumahan.png'); ?>" alt="Kerumah Tanggaan">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>KerumahTanggaan</h2>
+                                    <p>Pengadaan, Distribusi, Penggunaan.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            class="col-6 col-xl-3 py-0 d-flex flex-column justify-content-between justify-content-md-center align-items-center baris">
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/bankdarah.png'); ?>" alt="Bank Darah">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Bank Darah</h2>
+                                    <p>Order, Uji Silang, Stok Darah.</p>
+                                </div>
+                            </div>
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/cssd.png'); ?>" alt="CSSD">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>CSSD</h2>
+                                    <p>Produksi, Sterilisasi.</p>
+                                </div>
+                            </div>
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/ambulan.png'); ?>" alt="Ambulan">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Ambulan</h2>
+                                    <p>Order, Pengantaran Pasien.</p>
+                                </div>
+                            </div>
+                            <div class="box d-flex">
+                                <div class="foto d-lg-block d-none">
+                                    <img src="<?= base_url('assets/image/laundri.png'); ?>" alt="Laundry">
+                                </div>
+                                <div class="d-inline-block teks">
+                                    <h2>Laundry</h2>
+                                    <p>Order, Pembersihan Linen.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2 p-0"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-2 p-0"></div>
+        <div class="col-md p-0 simrs2">
+            <div class="simrs-konten simrsk2">
+                
             </div>
         </div>
         <div class="col-md-2 p-0"></div>
