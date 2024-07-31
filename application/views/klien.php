decc87b0d1d35555f7aa918f9500939e4c05510c
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-07-31 09:24:48 +0700

    KLIEN SELESAI, RESPONSIF

--- application/views/klien.php
+++ application/views/klien.php
@@ -1,12 +1,64 @@
 <div class="container-fluid klien" id="klien">
     <div class="row">
-        <div class="col text-center">
+        <div class="col text-center headklien d-flex justify-content-center">
             <p>Kami telah dipercaya sebagai solusi untuk kebutuhan Sistem Manajemen Rumah Sakit di berbagai rumah sakit
                 di
                 Indonesia.</p>
         </div>
     </div>
     <div class="row">
-
+        <div class="col d-flex justify-content-center align-items-center flex-wrap klogo">
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo1.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo2.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo3.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo4.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo5.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo6.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo7.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo8.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo9.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo10.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo11.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo12.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo13.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo14.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo15.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo16.png'); ?>" alt="Logo Klien">
+            </div>
+            <div class="klienlogo">
+                <img class="img-fluid" src="<?= base_url('assets/image/klogo17.png'); ?>" alt="Logo Klien">
+            </div>
+        </div>
     </div>
 </div>
